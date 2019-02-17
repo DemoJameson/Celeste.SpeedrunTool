f2bcd344ea3c1780ac270d373042fdec94ee124f
DemoJameson <demojameson@gmail.com>
2019-02-17 15:48:33 +0800

    Bug Fix: Ice ball always repspawn even if you step on it and save state.

--- SpeedrunTool/SaveLoad/Actions/FireBallAction.cs
+++ SpeedrunTool/SaveLoad/Actions/FireBallAction.cs
@@ -1,6 +1,8 @@
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.Xna.Framework;
+using Monocle;
 
 namespace Celeste.Mod.SpeedrunTool.SaveLoad.Actions
 {
@@ -10,10 +12,12 @@ namespace Celeste.Mod.SpeedrunTool.SaveLoad.Actions
 
         public override void OnQuickSave(Level level)
         {
-            _savedFireBalls = level.Tracker.GetCastEntities<FireBall>().ToDictionary(ball => ball.GetExtendedDataValue<string>("nodesIndexKey"));
+            _savedFireBalls = level.Tracker.GetCastEntities<FireBall>()
+                .ToDictionary(ball => ball.GetExtendedDataValue<string>("nodesIndexKey"));
         }
 
-        private void RestoreFireBallPosition(On.Celeste.FireBall.orig_ctor_Vector2Array_int_int_float_float orig, FireBall self,
+        private void RestoreFireBallState(On.Celeste.FireBall.orig_ctor_Vector2Array_int_int_float_float orig,
+            FireBall self,
             Vector2[] nodes, int amount, int index, float offset, float speedMult)
         {
             orig(self, nodes, amount, index, offset, speedMult);
@@ -23,9 +27,34 @@ namespace Celeste.Mod.SpeedrunTool.SaveLoad.Actions
 
             if (IsLoadStart && _savedFireBalls.ContainsKey(nodesIndexKey))
             {
-                self.CopyPrivateField("percent", _savedFireBalls[nodesIndexKey]);
+                FireBall savedFireBall = _savedFireBalls[nodesIndexKey];
+                self.CopyPrivateField("percent", savedFireBall);
+                
+                if ((bool) savedFireBall.GetPrivateField("iceMode"))
+                {
+                    self.Visible = self.Collidable = savedFireBall.Collidable;
+                    self.Add(new Coroutine(RestoreBroken(self, savedFireBall)));
+                }
             }
         }
+
+        private static IEnumerator RestoreBroken(FireBall self, FireBall savedFireBall)
+        {
+            self.CopyPrivateField("broken", savedFireBall);
+            yield break;
+        }
+
+        private void SpriteOnPlay(On.Monocle.Sprite.orig_Play orig, Sprite self, string id, bool restart,
+            bool randomizeFrame)
+        {
+            orig(self, id, restart, randomizeFrame);
+
+            if (id == "ice" && !restart && randomizeFrame && self.Entity is FireBall iceBall && iceBall.Collidable)
+            {
+                iceBall.Visible = true;
+            }
+        }
+
 
         public override void OnClear()
         {
@@ -34,12 +63,14 @@ namespace Celeste.Mod.SpeedrunTool.SaveLoad.Actions
 
         public override void OnLoad()
         {
-            On.Celeste.FireBall.ctor_Vector2Array_int_int_float_float += RestoreFireBallPosition;
+            On.Celeste.FireBall.ctor_Vector2Array_int_int_float_float += RestoreFireBallState;
+            On.Monocle.Sprite.Play += SpriteOnPlay;
         }
 
         public override void OnUnload()
         {
-            On.Celeste.FireBall.ctor_Vector2Array_int_int_float_float -= RestoreFireBallPosition;
+            On.Celeste.FireBall.ctor_Vector2Array_int_int_float_float -= RestoreFireBallState;
+            On.Monocle.Sprite.Play -= SpriteOnPlay;
         }
 
         public override void OnInit()

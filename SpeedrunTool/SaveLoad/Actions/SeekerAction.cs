879e4b1f9273cc2a99c3ab7be38c3e2a415b4af0
DemoJameson <demojameson@gmail.com>
2020-05-09 21:00:19 +0800

    支持保存恢复新浪雕像

--- SpeedrunTool/SaveLoad/Actions/SeekerAction.cs
+++ SpeedrunTool/SaveLoad/Actions/SeekerAction.cs
@@ -7,12 +7,17 @@ using Monocle;
 
 namespace Celeste.Mod.SpeedrunTool.SaveLoad.Actions {
     public class SeekerAction : AbstractEntityAction {
+        private const string RemoveStatue = "RemoveStatue";
         private const int StRegenerate = 6;
-        
+
         private readonly Dictionary<EntityID, Seeker> savedSeekers = new Dictionary<EntityID, Seeker>();
-        
+
+        private readonly Dictionary<EntityID, SeekerStatue> savedSeekerStatues =
+            new Dictionary<EntityID, SeekerStatue>();
+
         public override void OnQuickSave(Level level) {
             savedSeekers.AddRange(level.Entities.FindAll<Seeker>());
+            savedSeekerStatues.AddRange(level.Entities.FindAll<SeekerStatue>());
         }
 
         private void RestoreSeekerPosition(On.Celeste.Seeker.orig_ctor_EntityData_Vector2 orig, Seeker self,
@@ -20,16 +25,28 @@ namespace Celeste.Mod.SpeedrunTool.SaveLoad.Actions {
             EntityID entityId = data.ToEntityId();
             self.SetEntityId(entityId);
             orig(self, data, offset);
-
+            
             if (IsLoadStart) {
                 if (savedSeekers.ContainsKey(entityId)) {
                     Seeker savedSeeker = savedSeekers[self.GetEntityId()];
                     self.Position = savedSeeker.Position;
                     self.Add(new Coroutine(SetStateMachine(self, savedSeeker)));
-                }
-                else {
+                } else {
+                    self.Visible = false;
                     self.Add(new RemoveSelfComponent());
                 }
+            }
+        }
+
+        private void SeekerStatueOnCtor(On.Celeste.SeekerStatue.orig_ctor orig, SeekerStatue self, EntityData data,
+            Vector2 offset) {
+            EntityID entityId = data.ToEntityId();
+            self.SetEntityId(entityId);
+            self.SetEntityData(data);
+            orig(self, data, offset);
+
+            if (IsLoadStart && !savedSeekerStatues.ContainsKey(entityId)) {
+                self.SetExtendedBoolean(RemoveStatue, true);
             }
         }
 
@@ -44,16 +61,36 @@ namespace Celeste.Mod.SpeedrunTool.SaveLoad.Actions {
             yield break;
         }
 
+        private void SeekerStatueOnUpdate(On.Celeste.SeekerStatue.orig_Update orig, SeekerStatue self) {
+            if (self.GetExtendedBoolean(RemoveStatue)) {
+                self.SetExtendedBoolean(RemoveStatue, false);
+                if (savedSeekers.ContainsKey(self.GetEntityId())) {
+                    Seeker savedSeeker = savedSeekers[self.GetEntityId()];
+                    Seeker seeker = new Seeker(self.GetEntityData(), Vector2.Zero) {Position = savedSeeker.Position};
+                    seeker.Add(new Coroutine(SetStateMachine(seeker, savedSeeker)));
+                    self.Scene.Add(seeker);
+                }
+                self.RemoveSelf();
+                return;
+            }
+            
+            orig(self);
+        }
+
         public override void OnClear() {
             savedSeekers.Clear();
+            savedSeekerStatues.Clear();
         }
 
         public override void OnLoad() {
             On.Celeste.Seeker.ctor_EntityData_Vector2 += RestoreSeekerPosition;
+            On.Celeste.SeekerStatue.ctor += SeekerStatueOnCtor;
+            On.Celeste.SeekerStatue.Update += SeekerStatueOnUpdate;
         }
 
         public override void OnUnload() {
             On.Celeste.Seeker.ctor_EntityData_Vector2 -= RestoreSeekerPosition;
+            On.Celeste.SeekerStatue.ctor -= SeekerStatueOnCtor;
         }
     }
 }

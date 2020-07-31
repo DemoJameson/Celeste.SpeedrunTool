4ae26b8ce89e40ae82d8001616421b1fb52d7925
DemoJameson <demojameson@gmail.com>
2020-08-01 03:06:17 +0800

    Clean code.

--- SpeedrunTool/SaveLoad/AutoLoadStateUtils.cs
+++ SpeedrunTool/SaveLoad/AutoLoadStateUtils.cs
@@ -15,28 +15,29 @@ namespace Celeste.Mod.SpeedrunTool.SaveLoad {
 
         // Everest 的 Bug，另外的 Mod Hook 了 PlayerDeadBody.End 方法后 Level.DoScreenWipe Hook 的方法 wipeIn 为 false 时就不触发了
         // 所以改成了 Hook AreaData.DoScreenWipe 方法
-        private static void AutoLoadStateWhenDeath(On.Celeste.AreaData.orig_DoScreenWipe orig, AreaData self, Scene scene,
+        private static void AutoLoadStateWhenDeath(On.Celeste.AreaData.orig_DoScreenWipe orig, AreaData self,
+            Scene scene,
             bool wipeIn, Action onComplete) {
             if (SpeedrunToolModule.Settings.Enabled && Manager.IsSaved &&
                 !wipeIn && scene is Level level &&
-                onComplete != null && (onComplete == level.Reload || scene.Entities.FindFirst<PlayerDeadBody>()?.HasGolden == true)) {
+                onComplete != null && (onComplete == level.Reload ||
+                                       scene.Entities.FindFirst<PlayerDeadBody>()?.HasGolden == true)) {
                 Action origComplete = onComplete;
                 onComplete = () => {
                     // 避免死亡时读档后重复读档
-                    if(level.Entities.FindFirst<Player>() != null) return;
-                    origComplete();
-                };
-                if (SpeedrunToolModule.Settings.AutoLoadAfterDeath) {
-                    onComplete = () => {
-                        // 避免死亡时读档后重复读档
-                        if(level.Entities.FindFirst<Player>() != null) return;
+                    if (level.Entities.FindFirst<Player>() != null) return;
+
+                    if (SpeedrunToolModule.Settings.AutoLoadAfterDeath) {
+                        // double check 以防在死亡后清除了存档
                         if (Manager.IsSaved) {
                             Manager.LoadState();
                         } else {
                             origComplete();
                         }
-                    };
-                }
+                    } else {
+                        origComplete();
+                    }
+                };
             }
 
             orig(self, scene, wipeIn, onComplete);

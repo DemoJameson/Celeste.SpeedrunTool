0264b9223832bb8680b8964236fc8647d45a65c5
DemoJameson <demojameson@gmail.com>
2020-08-03 22:59:09 +0800

    死亡时自动读档的时机提前了

--- SpeedrunTool/SaveLoad/AutoLoadStateUtils.cs
+++ SpeedrunTool/SaveLoad/AutoLoadStateUtils.cs
@@ -1,46 +1,28 @@
-using System;
-using Monocle;
+using Celeste.Mod.SpeedrunTool.Extensions;
 
 namespace Celeste.Mod.SpeedrunTool.SaveLoad {
-    internal class AutoLoadStateUtils {
+    internal static class AutoLoadStateUtils {
         private static StateManager Manager => StateManager.Instance;
 
         public static void OnHook() {
-            On.Celeste.AreaData.DoScreenWipe += AutoLoadStateWhenDeath;
+            On.Celeste.PlayerDeadBody.End += PlayerDeadBodyOnEnd;
         }
 
         public static void OnUnhook() {
-            On.Celeste.AreaData.DoScreenWipe -= AutoLoadStateWhenDeath;
+            On.Celeste.PlayerDeadBody.End -= PlayerDeadBodyOnEnd;
         }
 
-        // Everest 的 Bug，另外的 Mod Hook 了 PlayerDeadBody.End 方法后 Level.DoScreenWipe Hook 的方法 wipeIn 为 false 时就不触发了
-        // 所以改成了 Hook AreaData.DoScreenWipe 方法
-        private static void AutoLoadStateWhenDeath(On.Celeste.AreaData.orig_DoScreenWipe orig, AreaData self,
-            Scene scene,
-            bool wipeIn, Action onComplete) {
-            if (SpeedrunToolModule.Settings.Enabled && Manager.IsSaved &&
-                !wipeIn && scene is Level level &&
-                onComplete != null && (onComplete == level.Reload ||
-                                       scene.Entities.FindFirst<PlayerDeadBody>()?.HasGolden == true)) {
-                Action origComplete = onComplete;
-                onComplete = () => {
-                    // 避免死亡时读档后重复读档
-                    if (level.Entities.FindFirst<Player>() != null) return;
-
-                    if (SpeedrunToolModule.Settings.AutoLoadAfterDeath) {
-                        // double check 以防在死亡后清除了存档
-                        if (Manager.IsSaved) {
-                            Manager.LoadState();
-                        } else {
-                            origComplete();
-                        }
-                    } else {
-                        origComplete();
-                    }
-                };
+        private static void PlayerDeadBodyOnEnd(On.Celeste.PlayerDeadBody.orig_End orig, PlayerDeadBody self) {
+            if (SpeedrunToolModule.Settings.Enabled
+                && SpeedrunToolModule.Settings.AutoLoadAfterDeath
+                && Manager.IsSaved
+                && !(bool) self.GetField("finished")
+            ) {
+                self.RemoveSelf();
+                self.SceneAs<Level>().OnEndOfFrame += () => Manager.LoadState();
+            } else {
+                orig(self);
             }
-
-            orig(self, scene, wipeIn, onComplete);
         }
     }
 }

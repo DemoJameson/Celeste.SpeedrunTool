f14bec15293f8e3eae3ddc603f43a22ee24ae2ec
DemoJameson <demojameson@gmail.com>
2020-08-22 13:58:26 +0800

    Refactor SaveLoadAction

--- SpeedrunTool/SaveLoad/SaveLoadAction.cs
+++ SpeedrunTool/SaveLoad/SaveLoadAction.cs
@@ -4,16 +4,16 @@ using Celeste.Mod.SpeedrunTool.Extensions;
 using FMOD.Studio;
 
 namespace Celeste.Mod.SpeedrunTool.SaveLoad {
-    public class SaveLoadAction {
+    public sealed class SaveLoadAction {
         private static readonly List<SaveLoadAction> All = new List<SaveLoadAction>();
 
-        private readonly Dictionary<string, object> savedValues = new Dictionary<string, object>();
-        private readonly Action<Dictionary<string, object>, Level> saveState;
-        private readonly Action<Dictionary<string, object>, Level> loadState;
+        private readonly Dictionary<Type, Dictionary<string, object>> savedValues = new Dictionary<Type, Dictionary<string, object>>();
+        private readonly Action<Dictionary<Type, Dictionary<string, object>>, Level> saveState;
+        private readonly Action<Dictionary<Type, Dictionary<string, object>>, Level> loadState;
 
         public SaveLoadAction(
-            Action<Dictionary<string, object>, Level> saveState,
-            Action<Dictionary<string, object>, Level> loadState) {
+            Action<Dictionary<Type, Dictionary<string, object>>, Level> saveState,
+            Action<Dictionary<Type, Dictionary<string, object>>, Level> loadState) {
             this.saveState = saveState;
             this.loadState = loadState;
         }
@@ -40,48 +40,54 @@ namespace Celeste.Mod.SpeedrunTool.SaveLoad {
             }
         }
 
-        private static void SaveStaticFieldValues(Dictionary<string, object> values, Type type,
+        private static void SaveStaticFieldValues(Dictionary<Type, Dictionary<string, object>> values, Type type,
             params string[] fieldNames) {
+            if (!values.ContainsKey(type)) {
+                values[type] = new Dictionary<string, object>();
+            }
+
             foreach (var fieldName in fieldNames) {
-                values[fieldName] = type.GetFieldValue(fieldName).DeepCloneShared();
+                values[type][fieldName] = type.GetFieldValue(fieldName).DeepCloneShared();
             }
         }
 
-        private static void LoadStaticFieldValues(Dictionary<string, object> values, Type type) {
-            foreach (string fieldName in values.Keys) {
-                type.SetFieldValue(fieldName, values[fieldName].DeepCloneShared());
+        private static void LoadStaticFieldValues(Dictionary<Type, Dictionary<string, object>> values) {
+            foreach (KeyValuePair<Type, Dictionary<string, object>> pair in values) {
+                foreach (string fieldName in pair.Value.Keys) {
+                    pair.Key.SetFieldValue(fieldName, pair.Value[fieldName].DeepCloneShared());
+                }
             }
         }
 
         internal static void OnLoad() {
-            // ExtendedVariantMode JumpCount
-            if (Type.GetType("ExtendedVariants.Variants.JumpCount, ExtendedVariantMode") is Type jumpCountType) {
-                All.Add(new SaveLoadAction(
-                    (savedValues, level) => { SaveStaticFieldValues(savedValues, jumpCountType, "jumpBuffer"); },
-                    (savedValues, level) => { LoadStaticFieldValues(savedValues, jumpCountType); }
-                ));
-            }
+            SupportAudioMusic();
+            SupportExtendedVariants();
+            SupportMaxHelpingHand();
+            SupportPandorasBox();
+        }
 
-            // MaxHelpingHand RainbowSpinnerColorController
-            if (Type.GetType("Celeste.Mod.MaxHelpingHand.Entities.RainbowSpinnerColorController, MaxHelpingHand") is Type colorControllerType
-            && Delegate.CreateDelegate(typeof(On.Celeste.CrystalStaticSpinner.hook_GetHue), colorControllerType.GetMethodInfo("getRainbowSpinnerHue")) is
-                On.Celeste.CrystalStaticSpinner.hook_GetHue hookGetHue
-            ) {
-                All.Add(new SaveLoadAction(
-                    (savedValues, level) => {
-                        SaveStaticFieldValues(savedValues, colorControllerType,
-                            "rainbowSpinnerHueHooked", "transitionProgress", "spinnerControllerOnScreen",
-                            "nextSpinnerController");
-                    },
-                    (savedValues, level) => {
-                        if ((bool) savedValues["rainbowSpinnerHueHooked"]) {
-                            On.Celeste.CrystalStaticSpinner.GetHue += hookGetHue;
-                        }
-                        LoadStaticFieldValues(savedValues, colorControllerType);
-                    }
-                ));
-            }
+        private static void SupportAudioMusic() {
+            // Audio Music
+            All.Add(new SaveLoadAction(
+                (savedValues, level) => {
+                    Dictionary<string, object> saved = new Dictionary<string, object>();
+                    saved.Add("CurrentMusic", Audio.CurrentMusic);
+                    saved.Add("CurrentAmbience", Audio.GetEventName(Audio.CurrentAmbienceEventInstance));
+                    saved.Add("CurrentAltMusic", Audio.GetEventName(typeof(Audio).GetFieldValue("currentAltMusicEvent") as EventInstance));
+                    saved.Add("MusicUnderwater", Audio.MusicUnderwater);
+                    savedValues[typeof(Audio)] = saved;
+                },
+                (savedValues, level) => {
+                    Dictionary<string, object> saved = savedValues[typeof(Audio)];
+                    Audio.SetMusic(saved["CurrentMusic"] as string);
+                    Audio.SetAmbience(saved["CurrentAmbience"] as string);
+                    Audio.SetAltMusic(saved["CurrentAltMusic"] as string);
+                    Audio.MusicUnderwater = (bool) saved["MusicUnderwater"];
+                }
+            ));
+        }
 
+        private static void SupportPandorasBox() {
             // PandorasBox TimeField
             if (Type.GetType("Celeste.Mod.PandorasBox.TimeField, PandorasBox") is Type timeFieldType
                 && Delegate.CreateDelegate(typeof(On.Celeste.Player.hook_Update), timeFieldType.GetMethodInfo("PlayerUpdateHook")) is
@@ -98,36 +104,57 @@ namespace Celeste.Mod.SpeedrunTool.SaveLoad {
                         // player.collider 的类型都变了
                         // System.Exception: Collisions against the collider type are not implemented!
                         // 在 Monocle.Collider.Collide(Collider collider)
-                        savedValues["-player-"] = timeFieldType.GetFieldValue("targetPlayer").GetPropertyValue("Target").DeepCloneShared();
-                        savedValues["-timeField-"] = timeFieldType.GetFieldValue("lingeringTarget").GetPropertyValue("Target").DeepCloneShared();
+                        savedValues[timeFieldType]["-player-"] =
+                            timeFieldType.GetFieldValue("targetPlayer").GetPropertyValue("Target").DeepCloneShared();
+                        savedValues[timeFieldType]["-timeField-"] =
+                            timeFieldType.GetFieldValue("lingeringTarget").GetPropertyValue("Target").DeepCloneShared();
                     },
                     (savedValues, level) => {
-                        if ((bool) savedValues["hookAdded"]) {
+                        if ((bool) savedValues[timeFieldType]["hookAdded"]) {
                             On.Celeste.Player.Update += hookUpdate;
                         }
-                        LoadStaticFieldValues(savedValues, timeFieldType);
-                        timeFieldType.GetFieldValue("targetPlayer").SetPropertyValue("Target", savedValues["-player-"].DeepCloneShared());
-                        timeFieldType.GetFieldValue("lingeringTarget").SetPropertyValue("Target", savedValues["-timeField-"].DeepCloneShared());
+
+                        LoadStaticFieldValues(savedValues);
+                        timeFieldType.GetFieldValue("targetPlayer")
+                            .SetPropertyValue("Target", savedValues[timeFieldType]["-player-"].DeepCloneShared());
+                        timeFieldType.GetFieldValue("lingeringTarget")
+                            .SetPropertyValue("Target", savedValues[timeFieldType]["-timeField-"].DeepCloneShared());
                     }
                 ));
             }
+        }
 
-            // Audio Music
-            All.Add(new SaveLoadAction(
-                (savedValues, level) => {
-                    savedValues.Add("CurrentMusic", Audio.CurrentMusic);
-                    savedValues.Add("CurrentAmbience", Audio.GetEventName(Audio.CurrentAmbienceEventInstance));
-                    savedValues.Add("CurrentAltMusic",
-                        Audio.GetEventName(typeof(Audio).GetFieldValue("currentAltMusicEvent") as EventInstance));
-                    savedValues.Add("MusicUnderwater", Audio.MusicUnderwater);
-                },
-                (savedValues, level) => {
-                    Audio.SetMusic(savedValues["CurrentMusic"] as string);
-                    Audio.SetAmbience(savedValues["CurrentAmbience"] as string);
-                    Audio.SetAltMusic(savedValues["CurrentAltMusic"] as string);
-                    Audio.MusicUnderwater = (bool) savedValues["MusicUnderwater"];
-                }
-            ));
+        private static void SupportMaxHelpingHand() {
+            // MaxHelpingHand RainbowSpinnerColorController
+            if (Type.GetType("Celeste.Mod.MaxHelpingHand.Entities.RainbowSpinnerColorController, MaxHelpingHand") is Type colorControllerType
+                && Delegate.CreateDelegate(typeof(On.Celeste.CrystalStaticSpinner.hook_GetHue),
+                        colorControllerType.GetMethodInfo("getRainbowSpinnerHue")) is
+                    On.Celeste.CrystalStaticSpinner.hook_GetHue hookGetHue
+            ) {
+                All.Add(new SaveLoadAction(
+                    (savedValues, level) => {
+                        SaveStaticFieldValues(savedValues, colorControllerType,
+                            "rainbowSpinnerHueHooked", "transitionProgress", "spinnerControllerOnScreen",
+                            "nextSpinnerController");
+                    },
+                    (savedValues, level) => {
+                        if ((bool) savedValues[colorControllerType]["rainbowSpinnerHueHooked"]) {
+                            On.Celeste.CrystalStaticSpinner.GetHue += hookGetHue;
+                        }
+                        LoadStaticFieldValues(savedValues);
+                    }
+                ));
+            }
+        }
+
+        private static void SupportExtendedVariants() {
+            // ExtendedVariantMode JumpCount
+            if (Type.GetType("ExtendedVariants.Variants.JumpCount, ExtendedVariantMode") is Type jumpCountType) {
+                All.Add(new SaveLoadAction(
+                    (savedValues, level) => { SaveStaticFieldValues(savedValues, jumpCountType, "jumpBuffer"); },
+                    (savedValues, level) => { LoadStaticFieldValues(savedValues); }
+                ));
+            }
         }
 
         internal static void OnUnload() {

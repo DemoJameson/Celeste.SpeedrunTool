78db8d9053c17601757d3f68beb567addd5abf3b
DemoJameson <demojameson@gmail.com>
2020-09-02 10:27:34 +0800

    Make CloneProcessor thread safety.

--- SpeedrunTool/SaveLoad/DeepClonerUtils.cs
+++ SpeedrunTool/SaveLoad/DeepClonerUtils.cs
@@ -51,64 +51,68 @@ namespace Celeste.Mod.SpeedrunTool.SaveLoad {
             // Clone 对象的字段前，判断哪些类型是直接使用原对象或者自行通过其它方法 clone
             // Before cloning object's field, determine which types are directly used by the original object
             DeepCloner.AddPreCloneProcessor((sourceObj, deepCloneState) => {
-                if (sourceObj is Level) {
-                    // 金草莓死亡或者 PageDown/Up 切换房间后等等改变 Level 实例的情况
-                    // After golden strawberry deaths or changing rooms w/ Page Down / Up
-                    if (Engine.Scene is Level level) return level;
-                    return sourceObj;
-                }
-
-                if (sourceObj is Entity entity
-                    && entity.TagCheck(Tags.Global)
-                    && !(entity is CassetteBlockManager)
-                    && !(entity is SeekerBarrierRenderer)
-                    && !(entity is LightningRenderer)
-                    // Fixes: Glyph Teleport Area Effect
-                    && entity.GetType().FullName != "Celeste.Mod.AcidHelper.Entities.InstantTeleporterRenderer"
-                ) return sourceObj;
-
-                // 稍后重新创建正在播放的 SoundSource 里的 EventInstance 实例
-                if (sourceObj is SoundSource source
-                    // TODO SoundEmitter 的声音会存留在关卡中，切换房间后保存依然会播放
-                    && !(source.Entity is SoundEmitter)
-                    && source.Playing
-                    && source.GetFieldValue("instance") is EventInstance instance) {
-                    if (string.IsNullOrEmpty(source.EventName)) return null;
-                    instance.NeedManualClone(true);
-                    instance.SaveTimelinePosition(instance.LoadTimelinePosition());
-
-                    return null;
-                }
-
-                if (sourceObj is CassetteBlockManager manager) {
-                    // isLevelMusic = true 时 sfx 自动等于 Audio.CurrentMusicEventInstance，无需重建
-                    if (manager.GetFieldValue("sfx") is EventInstance sfx &&
-                        !(bool) manager.GetFieldValue("isLevelMusic")) {
-                        sfx.NeedManualClone(true);
+                lock (sourceObj) {
+                    if (sourceObj is Level) {
+                        // 金草莓死亡或者 PageDown/Up 切换房间后等等改变 Level 实例的情况
+                        // After golden strawberry deaths or changing rooms w/ Page Down / Up
+                        if (Engine.Scene is Level level) return level;
+                        return sourceObj;
                     }
 
-                    if (manager.GetFieldValue("snapshot") is EventInstance snapshot) {
-                        snapshot.NeedManualClone(true);
+                    if (sourceObj is Entity entity
+                        && entity.TagCheck(Tags.Global)
+                        && !(entity is CassetteBlockManager)
+                        && !(entity is SeekerBarrierRenderer)
+                        && !(entity is LightningRenderer)
+                        // Fixes: Glyph Teleport Area Effect
+                        && entity.GetType().FullName != "Celeste.Mod.AcidHelper.Entities.InstantTeleporterRenderer"
+                    ) return sourceObj;
+
+                    lock (sourceObj) {
+                        // 稍后重新创建正在播放的 SoundSource 里的 EventInstance 实例
+                        if (sourceObj is SoundSource source
+                            // TODO SoundEmitter 的声音会存留在关卡中，切换房间后保存依然会播放
+                            && !(source.Entity is SoundEmitter)
+                            && source.Playing
+                            && source.GetFieldValue("instance") is EventInstance instance) {
+                            if (string.IsNullOrEmpty(source.EventName)) return null;
+                            instance.NeedManualClone(true);
+                            instance.SaveTimelinePosition(instance.LoadTimelinePosition());
+
+                            return null;
+                        }
+
+                        if (sourceObj is CassetteBlockManager manager) {
+                            // isLevelMusic = true 时 sfx 自动等于 Audio.CurrentMusicEventInstance，无需重建
+                            if (manager.GetFieldValue("sfx") is EventInstance sfx &&
+                                !(bool) manager.GetFieldValue("isLevelMusic")) {
+                                sfx.NeedManualClone(true);
+                            }
+
+                            if (manager.GetFieldValue("snapshot") is EventInstance snapshot) {
+                                snapshot.NeedManualClone(true);
+                            }
+
+                            return null;
+                        }
+
+                        // 重新创建正在播放的 EventInstance 实例
+                        if (sourceObj is EventInstance eventInstance && eventInstance.IsNeedManualClone()) {
+                            return eventInstance.Clone();
+                        }
+
+                        // Fixes: 克隆 WeakReference 后 Target 没有一起被克隆的问题，修复 dynData.Weak 克隆不完整导致的一些报错
+                        // System.Reflection.TargetException: 非静态字段需要一个目标。
+                        // 在 System.Reflection.RtFieldInfo.CheckConsistency(Object target)
+                        // 在 System.Reflection.RtFieldInfo.InternalGetValue(Object obj, StackCrawlMark& stackMark)
+                        // 在 System.Reflection.RtFieldInfo.GetValue(Object obj)
+                        // 在 MonoMod.Utils.DynData`1.<>c__DisplayClass19_0.<.cctor>b__1(TTarget obj)
+                        // 在 MonoMod.Utils.DynData`1.get_Item(String name)
+                        // 在 Celeste.Mod.MaxHelpingHand.Entities.CustomizableRefill.<>c__DisplayClass0_0.<.ctor>b__0(Player player)
+                        if (sourceObj is WeakReference sourceWeak) {
+                            return new WeakReference(sourceWeak.Target.DeepClone(deepCloneState));
+                        }
                     }
-
-                    return null;
-                }
-
-                // 重新创建正在播放的 EventInstance 实例
-                if (sourceObj is EventInstance eventInstance && eventInstance.IsNeedManualClone()) {
-                    return eventInstance.Clone();
-                }
-
-                // Fixes: 克隆 WeakReference 后 Target 没有一起被克隆的问题，修复 dynData.Weak 克隆不完整导致的一些报错
-                // System.Reflection.TargetException: 非静态字段需要一个目标。
-                // 在 System.Reflection.RtFieldInfo.CheckConsistency(Object target)
-                // 在 System.Reflection.RtFieldInfo.InternalGetValue(Object obj, StackCrawlMark& stackMark)
-                // 在 System.Reflection.RtFieldInfo.GetValue(Object obj)
-                // 在 MonoMod.Utils.DynData`1.<>c__DisplayClass19_0.<.cctor>b__1(TTarget obj)
-                // 在 MonoMod.Utils.DynData`1.get_Item(String name)
-                // 在 Celeste.Mod.MaxHelpingHand.Entities.CustomizableRefill.<>c__DisplayClass0_0.<.ctor>b__0(Player player)
-                if (sourceObj is WeakReference sourceWeak) {
-                    return new WeakReference(sourceWeak.Target.DeepClone(deepCloneState));
                 }
 
                 return null;
@@ -119,60 +123,62 @@ namespace Celeste.Mod.SpeedrunTool.SaveLoad {
             DeepCloner.AddPostCloneProcessor((sourceObj, clonedObj, deepCloneState) => {
                 if (clonedObj == null) return null;
 
-                // 修复：DeepClone 的 hashSet.Containes(里面存在的引用对象) 总是返回 False，Dictionary 无此问题
-                // 原因：没有重写 GetHashCode 方法 https://github.com/force-net/DeepCloner/issues/17#issuecomment-678650032
-                // Fix: DeepClone's hashSet.Contains (ReferenceType) always returns false, Dictionary has no such problem
-                if (clonedObj.GetType().IsHashSet(out Type hashSetElementType) && !hashSetElementType.IsSimple()) {
-                    IEnumerator enumerator = ((IEnumerable) clonedObj).GetEnumerator();
+                lock (sourceObj) {
+                    // 修复：DeepClone 的 hashSet.Containes(里面存在的引用对象) 总是返回 False，Dictionary 无此问题
+                    // 原因：没有重写 GetHashCode 方法 https://github.com/force-net/DeepCloner/issues/17#issuecomment-678650032
+                    // Fix: DeepClone's hashSet.Contains (ReferenceType) always returns false, Dictionary has no such problem
+                    if (clonedObj.GetType().IsHashSet(out Type hashSetElementType) && !hashSetElementType.IsSimple()) {
+                        IEnumerator enumerator = ((IEnumerable) clonedObj).GetEnumerator();
 
-                    List<object> backup = new List<object>();
-                    while (enumerator.MoveNext()) {
-                        backup.Add(enumerator.Current);
+                        List<object> backup = new List<object>();
+                        while (enumerator.MoveNext()) {
+                            backup.Add(enumerator.Current);
+                        }
+
+                        if (backup.Count == 0) return clonedObj;
+
+                        clonedObj.InvokeMethod("Clear");
+                        backup.ForEach(obj => { clonedObj.InvokeMethod("Add", obj); });
                     }
 
-                    if (backup.Count == 0) return clonedObj;
+                    // 同上
+                    if (clonedObj.GetType().IsDictionary(out Type dictKeyType, out Type _)
+                        && !dictKeyType.IsSimple() && clonedObj is IDictionary clonedDict && clonedDict.Count > 0
+                    ) {
+                        Dictionary<object, object> backupDict = new Dictionary<object, object>();
+                        backupDict.AddRange(clonedDict);
+                        clonedDict.Clear();
+                        clonedDict.AddRange(backupDict);
+                    }
 
-                    clonedObj.InvokeMethod("Clear");
-                    backup.ForEach(obj => { clonedObj.InvokeMethod("Add", obj); });
-                }
+                    // LightingRenderer 需要，不然不会发光
+                    if (clonedObj is VertexLight vertexLight) {
+                        vertexLight.Index = -1;
+                    }
 
-                // 同上
-                if (clonedObj.GetType().IsDictionary(out Type dictKeyType, out Type _)
-                    && !dictKeyType.IsSimple() && clonedObj is IDictionary clonedDict && clonedDict.Count > 0
-                ) {
-                    Dictionary<object, object> backupDict = new Dictionary<object, object>();
-                    backupDict.AddRange(clonedDict);
-                    clonedDict.Clear();
-                    clonedDict.AddRange(backupDict);
-                }
+                    // Clone dynData.Data
+                    Type objType = sourceObj.GetType();
+                    if (objType.IsClass) {
+                        do {
+                            IDictionary dataMap = DynDataUtils.GetDataMap(objType);
+                            if (dataMap == null || dataMap.Count == 0) continue;
+                            WeakReference sourceWeak = new WeakReference(sourceObj);
 
-                // LightingRenderer 需要，不然不会发光
-                if (clonedObj is VertexLight vertexLight) {
-                    vertexLight.Index = -1;
-                }
+                            if (!dataMap.Contains(sourceWeak)) continue;
+                            if (!(dataMap[sourceWeak].GetFieldValue("Data") is Dictionary<string, object> data) || data.Count == 0) continue;
 
-                // Clone dynData.Data
-                Type objType = sourceObj.GetType();
-                if (objType.IsClass) {
-                    do {
-                        IDictionary dataMap = DynDataUtils.GetDataMap(objType);
-                        if (dataMap == null || dataMap.Count == 0) continue;
-                        WeakReference sourceWeak = new WeakReference(sourceObj);
+                            WeakReference clonedWeak = new WeakReference(clonedObj);
+                            dataMap[clonedWeak] = dataMap[sourceWeak].DeepClone(deepCloneState);
+                        } while ((objType = objType.BaseType) != null && objType.IsSameOrSubclassOf(typeof(object)));
+                    }
 
-                        if (!dataMap.Contains(sourceWeak)) continue;
-                        if (!(dataMap[sourceWeak].GetFieldValue("Data") is Dictionary<string, object> data) || data.Count == 0) continue;
+                    if (clonedObj is VirtualTexture virtualTexture && virtualTexture.IsDisposed) {
+                        virtualTexture.Reload();
+                    }
 
-                        WeakReference clonedWeak = new WeakReference(clonedObj);
-                        dataMap[clonedWeak] = dataMap[sourceWeak].DeepClone(deepCloneState);
-                    } while ((objType = objType.BaseType) != null && objType.IsSameOrSubclassOf(typeof(object)));
-                }
-
-                if (clonedObj is VirtualTexture virtualTexture && virtualTexture.IsDisposed) {
-                    virtualTexture.Reload();
-                }
-
-                if (clonedObj is VirtualRenderTarget virtualRenderTarget && virtualRenderTarget.IsDisposed) {
-                    virtualRenderTarget.Reload();
+                    if (clonedObj is VirtualRenderTarget virtualRenderTarget && virtualRenderTarget.IsDisposed) {
+                        virtualRenderTarget.Reload();
+                    }
                 }
 
                 return clonedObj;

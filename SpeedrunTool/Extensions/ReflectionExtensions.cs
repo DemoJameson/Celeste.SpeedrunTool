3dd11cc18002ffde78f738963e6caece4f38159d
DemoJameson <demojameson@gmail.com>
2020-12-23 17:08:34 +0800

    修复：SL时因为多线程问题导致游戏奔溃

--- SpeedrunTool/Extensions/ReflectionExtensions.cs
+++ SpeedrunTool/Extensions/ReflectionExtensions.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using Microsoft.Xna.Framework;
-using MonoMod.Utils;
 
 namespace Celeste.Mod.SpeedrunTool.Extensions {
     internal static class ReflectionExtensions {

41ca1a2c403f6ec22bcde7663ddaa71cc4227e4e
DemoJameson <demojameson@gmail.com>
2021-09-29 22:08:52 +0800

    fix: XNA 版本点击任务栏游戏图标缩小窗口后无法静音

--- SpeedrunTool/Source/Other/MuteInBackground.cs
+++ SpeedrunTool/Source/Other/MuteInBackground.cs
@@ -1,45 +1,77 @@
+using System;
 using Celeste.Mod.SpeedrunTool.Extensions;
 using Monocle;
 
 namespace Celeste.Mod.SpeedrunTool.Other {
     public static class MuteInBackground {
-        private static bool lastActive;
-        private static bool lastSetting;
+        private static bool init;
         private static SpeedrunToolSettings ModSettings => SpeedrunToolModule.Settings;
 
         [Load]
         private static void Load() {
-            On.Monocle.Scene.BeforeUpdate += SceneOnBeforeUpdate;
+            On.Celeste.Audio.Init += AudioOnInit;
+            On.Celeste.Audio.Unload += AudioOnUnload;
         }
 
         [Unload]
         private static void Unload() {
-            On.Monocle.Scene.BeforeUpdate -= SceneOnBeforeUpdate;
+            On.Celeste.Audio.Init -= AudioOnInit;
+            On.Celeste.Audio.Unload -= AudioOnUnload;
+            Engine.Instance.Window.ClientSizeChanged -= WindowOnClientSizeChanged;
+            Engine.Instance.Activated -= InstanceOnActivated;
+            Engine.Instance.Deactivated -= InstanceOnDeactivated;
         }
 
-        private static void SceneOnBeforeUpdate(On.Monocle.Scene.orig_BeforeUpdate orig, Scene self) {
-            orig(self);
+        /*延迟添加这些事件，不然 FNA 版本启动崩溃
+        System.NullReferenceException: Object reference not set to an instance of an object.
+        at Celeste.Audio.VCAVolume(String path, Nullable`1 volume)*/
+        private static void AudioOnInit(On.Celeste.Audio.orig_Init orig) {
+            orig();
 
-            if (ModSettings.MuteInBackground && lastActive != Engine.Instance.IsActive) {
-                if (Engine.Instance.IsActive) {
-                    RestoreAudio();
-                } else {
-                    Audio.MusicVolume = 0f;
-                    Audio.SfxVolume = 0f;
-                }
+            if (!init) {
+                init = true;
+                Engine.Instance.Window.ClientSizeChanged += WindowOnClientSizeChanged;
+                Engine.Instance.Activated += InstanceOnActivated;
+                Engine.Instance.Deactivated += InstanceOnDeactivated;
             }
+        }
 
-            if (lastSetting != ModSettings.MuteInBackground && !ModSettings.MuteInBackground) {
+        private static void AudioOnUnload(On.Celeste.Audio.orig_Unload orig) {
+            orig();
+            init = false;
+            Engine.Instance.Window.ClientSizeChanged -= WindowOnClientSizeChanged;
+            Engine.Instance.Activated -= InstanceOnActivated;
+            Engine.Instance.Deactivated -= InstanceOnDeactivated;
+        }
+
+        private static void InstanceOnActivated(object sender, EventArgs e) {
+            RestoreAudio();
+        }
+
+        private static void InstanceOnDeactivated(object sender, EventArgs e) {
+            MuteAudio();
+        }
+
+        private static void WindowOnClientSizeChanged(object sender, EventArgs e) {
+            if (Engine.Instance.Window.ClientBounds.Width == 0) {
+                MuteAudio();
+            } else {
                 RestoreAudio();
             }
+        }
 
-            lastActive = Engine.Instance.IsActive;
-            lastSetting = ModSettings.MuteInBackground;
+        private static void MuteAudio() {
+            if (ModSettings.MuteInBackground) {
+                Audio.MusicVolume = 0f;
+                Audio.SfxVolume = 0f;
+            }
         }
 
         private static void RestoreAudio() {
-            Settings.Instance.ApplyMusicVolume();
-            Settings.Instance.ApplySFXVolume();
+            if (ModSettings.MuteInBackground) {
+                Settings.Instance.ApplyMusicVolume();
+                Settings.Instance.ApplySFXVolume();
+            }
         }
     }
 }

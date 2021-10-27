b98fb5d27f6742fa676bbcbf6dbde1a2b3cc982a
DemoJameson <demojameson@gmail.com>
2021-10-27 21:07:00 +0800

    feat: 仅当ST启用时才应用冲刺白线修复

--- SpeedrunTool/Source/Other/VanillaBugFixer.cs
+++ SpeedrunTool/Source/Other/VanillaBugFixer.cs
@@ -16,7 +16,7 @@ namespace Celeste.Mod.SpeedrunTool.Other {
 
         // 修复：面向左边不按方向键冲刺，白色线条绘制异常
         private static SlashFx SlashFxOnBurst(On.Celeste.SlashFx.orig_Burst orig, Vector2 position, float direction) {
-            if (Math.Abs(direction + 3.141593) < 0.000001f) {
+            if (SpeedrunToolModule.Settings.Enabled && Math.Abs(direction + 3.141593) < 0.000001f) {
                 direction = -direction;
             }
 

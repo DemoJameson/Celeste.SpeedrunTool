6e70fde8fb8e5fc25a84e4f98f534692e3a2a2b6
DemoJameson <demojameson@gmail.com>
2022-09-16 15:36:05 +0800

    chore: bump v3.16.0

--- SpeedrunTool/Source/SpeedrunToolInterop.cs
+++ SpeedrunTool/Source/SpeedrunToolInterop.cs
@@ -1,6 +1,5 @@
 using Celeste.Mod.SpeedrunTool.SaveLoad;
 using MonoMod.ModInterop;
-using System.Collections.Generic;
 
 namespace Celeste.Mod.SpeedrunTool;
 
@@ -10,6 +9,7 @@ public static class SpeedrunToolInterop {
         typeof(SaveLoadExports).ModInterop();
     }
 
+    // ReSharper disable once MemberCanBePrivate.Global
     [ModExportName("SpeedrunTool.SaveLoad")]
     public static class SaveLoadExports {
         public static void RegisterStaticTypes(Type t, params string[] memberNames) {

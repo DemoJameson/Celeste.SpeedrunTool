4be3c10bb6037a07962d26f2466674052293872a
DemoJameson <demojameson@gmail.com>
2023-03-05 13:45:57 +0800

    fix: try fixing a weird savestate crash
    https://discord.com/channels/403698615446536203/666197738026827786/1081793364875288696

--- SpeedrunTool/Source/Utils/TasUtils.cs
+++ SpeedrunTool/Source/Utils/TasUtils.cs
@@ -6,7 +6,7 @@ namespace Celeste.Mod.SpeedrunTool.Utils;
 internal static class TasUtils {
     private static bool installed;
     private static bool running => Manager.Running;
-    private static bool showGamePlay => CelesteTasSettings.Instance.ShowGameplay;
+    private static bool showGamePlay => CelesteTasSettings.Instance?.ShowGameplay ?? true;
     public static bool Running => installed && running;
     public static bool HideGamePlay => installed && !showGamePlay;
 

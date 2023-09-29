6eeca6f5e8f69c3146803257a2e971943581166a
DemoJameson <demojameson@gmail.com>
2023-09-29 18:00:27 +0800

    feat: clear static variables when closing menu

--- SpeedrunTool/Source/SpeedrunToolMenu.cs
+++ SpeedrunTool/Source/SpeedrunToolMenu.cs
@@ -14,6 +14,7 @@ public static class SpeedrunToolMenu {
     private static List<EaseInSubMenu> options;
 
     public static void Create(TextMenu menu, bool inGame, EventInstance snapshot) {
+        menu.OnClose += () => options = null;
         menu.Add(new TextMenu.OnOff(Dialog.Clean(DialogIds.Enabled), ModSettings.Enabled).Change(value => {
             ModSettings.Enabled = value;
             foreach (EaseInSubMenu item in options) {

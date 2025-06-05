23fbafced32da44d39c3bc78dd254f7db9949197
Benjamin <benjamin@benjaminhalko.dev>
2025-06-05 14:46:43 -0700

    fix: motion-smoothing compatibility
    Removes motion-smoothing's SaveLoad hook if it has already been registered.
    
    This should address the crashes that occur when motion-smoothing is installed.

--- SpeedrunTool/Source/SaveLoad/ThirdPartySupport/MotionSmoothingFix.cs
+++ SpeedrunTool/Source/SaveLoad/ThirdPartySupport/MotionSmoothingFix.cs
@@ -1,5 +1,6 @@
 using Celeste.Mod.SpeedrunTool.Utils;
 using Mono.Cecil.Cil;
+using System.Collections.Generic;
 using System.Reflection;
 
 namespace Celeste.Mod.SpeedrunTool.SaveLoad.ThirdPartySupport;
@@ -25,6 +26,19 @@ internal static class MotionSmoothingFix {
             MotionSmoothingInstalled = true;
             handler = motionSmoothHandler;
             method = smoothAll;
+
+            // Remove MotionSmoothing's action from the SharedActions list
+           FieldInfo sharedActionsField = typeof(SaveLoadAction).GetField("SharedActions", BindingFlags.Static | BindingFlags.NonPublic);
+            if (sharedActionsField?.GetValue(null) is List<SaveLoadAction> sharedActions) {
+                foreach (SaveLoadAction action in sharedActions) {
+                    if (action.GetType().GetField("loadState", BindingFlags.NonPublic | BindingFlags.Instance)?.GetValue(action) is Delegate loadStateDelegate) {
+                        if (loadStateDelegate.Target?.GetType().Assembly.GetName().Name == "MotionSmoothing") {
+                            sharedActions.Remove(action);
+                            break;
+                        }
+                    }
+                };
+            }
 
             // the original one is a method call of a singleton, which may be wrong after deepclone?
             // (note save load actions are deepcloned to each save slot)

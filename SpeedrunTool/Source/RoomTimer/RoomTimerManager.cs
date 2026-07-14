DemoJameson/Celeste.SpeedrunTool

commit 6f57cca3f053a01b212e6fac704c407d20170310

use everest events

SpeedrunTool/Source/RoomTimer/RoomTimerManager.cs
@@ -41,7 +41,7 @@ public static class RoomTimerManager {
     private static void Load() {
         IL.Celeste.SpeedrunTimerDisplay.Update += SpeedrunTimerDisplayOnUpdate;
         On.Celeste.SpeedrunTimerDisplay.Render += Render;
-        On.Celeste.Level.Update += Timing;
+        Everest.Events.Level.OnAfterUpdate += Timing;
         On.Celeste.SummitCheckpoint.Update += UpdateTimerStateOnTouchFlag;
         On.Celeste.HeartGem.RegisterAsCollected += HeartGemOnRegisterAsCollected;
         On.Celeste.SaveData.RegisterCassette += SaveDataOnRegisterCassette;
@@ -56,7 +56,7 @@ public static class RoomTimerManager {
     private static void Unload() {
         IL.Celeste.SpeedrunTimerDisplay.Update -= SpeedrunTimerDisplayOnUpdate;
         On.Celeste.SpeedrunTimerDisplay.Render -= Render;
-        On.Celeste.Level.Update -= Timing;
+        Everest.Events.Level.OnAfterUpdate -= Timing;
         On.Celeste.SummitCheckpoint.Update -= UpdateTimerStateOnTouchFlag;
         On.Celeste.HeartGem.RegisterAsCollected -= HeartGemOnRegisterAsCollected;
         On.Celeste.SaveData.RegisterCassette -= SaveDataOnRegisterCassette;
@@ -154,9 +154,7 @@ public static class RoomTimerManager {
         }
     }
 
-    private static void Timing(On.Celeste.Level.orig_Update orig, Level self) {
-        orig(self);
-
+    private static void Timing(Level self) {
         string currentRoom = self.Session.Level;
         bool enteringNextRoom = previousRoom != null && previousRoom != currentRoom;
         previousRoom = currentRoom;

SpeedrunTool/Source/Other/AllowPauseDuringDeath.cs
@@ -7,18 +7,16 @@ namespace Celeste.Mod.SpeedrunTool.Other;
 public static class AllowPauseDuringDeath {
     [Load]
     private static void Load() {
-        On.Celeste.Level.Update += LevelOnUpdate;
+        Everest.Events.Level.OnAfterUpdate += LevelOnUpdate;
         typeof(Level).GetMethodInfo("orig_Pause").ILHook(DisableRetryMenu);
     }
 
     [Unload]
     private static void Unload() {
-        On.Celeste.Level.Update -= LevelOnUpdate;
+        Everest.Events.Level.OnAfterUpdate -= LevelOnUpdate;
     }
 
-    private static void LevelOnUpdate(On.Celeste.Level.orig_Update orig, Level level) {
-        orig(level);
-
+    private static void LevelOnUpdate(Level level) {
         if (!ModSettings.Enabled || !ModSettings.AllowPauseDuringDeath) {
             return;
         }

SpeedrunTool/Source/SpeedrunToolModule.cs
@@ -44,7 +44,8 @@ public class SpeedrunToolModule : EverestModule {
             "/ ModInterop 加入 IgnoreSaveLoad/ReturnSameObject(type) 的版本" +
             "       这里如果 type 是 entity 就处理完 Scene 之后原样返回 (PreClone 阶段), 否则直接原样返回 (KnownType 阶段)" +
             "/ 引入 Trigger, 使得可以告诉 SRT 哪些关卡是可以安全地存档的 (LuaCutscene 相关). 并在 SRT 发布时告诉 jesss#6307" +
-            "       可能此事也加入 ModInterop ";
+            "       可能此事也加入 ModInterop" +
+            "/ 引入 Trigger 或者某种全局实体, 使得可以告诉 SRT 传送的顺序";
         
         throw new NotImplementedException(todo);
 #endif

commit 287ddbe9c77664364dd1a5079955f5e5c559c9b7

rename some methods bugfix: PB timer won't show during the freeze of load state (caused by 6f57cca3)

SpeedrunTool/Source/RoomTimer/RoomTimerManager.cs
@@ -41,7 +41,7 @@ public static class RoomTimerManager {
     private static void Load() {
         IL.Celeste.SpeedrunTimerDisplay.Update += SpeedrunTimerDisplayOnUpdate;
         On.Celeste.SpeedrunTimerDisplay.Render += Render;
-        Everest.Events.Level.OnAfterUpdate += Timing;
+        SaveLoad.StateManager.OnAfterUpdate_EvenIfGameFreeze += Timing;
         On.Celeste.SummitCheckpoint.Update += UpdateTimerStateOnTouchFlag;
         On.Celeste.HeartGem.RegisterAsCollected += HeartGemOnRegisterAsCollected;
         On.Celeste.SaveData.RegisterCassette += SaveDataOnRegisterCassette;
@@ -56,7 +56,7 @@ public static class RoomTimerManager {
     private static void Unload() {
         IL.Celeste.SpeedrunTimerDisplay.Update -= SpeedrunTimerDisplayOnUpdate;
         On.Celeste.SpeedrunTimerDisplay.Render -= Render;
-        Everest.Events.Level.OnAfterUpdate -= Timing;
+        StateManager.OnAfterUpdate_EvenIfGameFreeze -= Timing;
         On.Celeste.SummitCheckpoint.Update -= UpdateTimerStateOnTouchFlag;
         On.Celeste.HeartGem.RegisterAsCollected -= HeartGemOnRegisterAsCollected;
         On.Celeste.SaveData.RegisterCassette -= SaveDataOnRegisterCassette;

SpeedrunTool/Source/SaveLoad/StateManager.cs
@@ -61,12 +61,15 @@ public sealed class StateManager {
 
     private readonly HashSet<EventInstance> playingEventInstances = [];
 
+    // different with Everest's event, which won't work when game freeze
+    internal static event Action<Level> OnAfterUpdate_EvenIfGameFreeze = null;
+
     #region Hook
 
     // manually call this to ensure it's first called
     internal static void Load() {
-        On.Monocle.Scene.BeforeUpdate += MakeGameFreezeAfterSaveLoad;
-        On.Celeste.Level.Update += UpdateBackdropWhenWaiting;
+        On.Celeste.Level.Update += MakeGameFreezeAfterSaveLoad;
+        On.Monocle.Scene.BeforeUpdate += UnfreezeTheGame;
         On.Celeste.PlayerDeadBody.End += AutoLoadStateWhenDeath;
         IL.Celeste.Level.TransitionRoutine += IL_LevelTransitionRoutine;
         On.Celeste.Level.TransitionRoutine += On_LevelTransitionRoutine;
@@ -80,8 +83,8 @@ public sealed class StateManager {
     }
 
     internal static void Unload() {
-        On.Monocle.Scene.BeforeUpdate -= MakeGameFreezeAfterSaveLoad;
-        On.Celeste.Level.Update -= UpdateBackdropWhenWaiting;
+        On.Celeste.Level.Update -= MakeGameFreezeAfterSaveLoad;
+        On.Monocle.Scene.BeforeUpdate -= UnfreezeTheGame;
         On.Celeste.PlayerDeadBody.End -= AutoLoadStateWhenDeath;
         IL.Celeste.Level.TransitionRoutine -= IL_LevelTransitionRoutine;
         On.Celeste.Level.TransitionRoutine -= On_LevelTransitionRoutine;
@@ -140,7 +143,7 @@ public sealed class StateManager {
         return !lastCheck;
     }
 
-    private static void MakeGameFreezeAfterSaveLoad(On.Monocle.Scene.orig_BeforeUpdate orig, Scene self) {
+    private static void UnfreezeTheGame(On.Monocle.Scene.orig_BeforeUpdate orig, Scene self) {
         if (ModSettings.Enabled && self is Level level && Instance.State == State.Waiting) {
             if (unfreezeInputs.Any(Instance.IsUnfreeze) || Hotkey.CheckDeathStatistics.Pressed()) {
                 Instance.lastChecks.Clear();
@@ -155,18 +158,23 @@ public sealed class StateManager {
         orig(self);
     }
 
-    private static void UpdateBackdropWhenWaiting(On.Celeste.Level.orig_Update orig, Level level) {
+    private static void MakeGameFreezeAfterSaveLoad(On.Celeste.Level.orig_Update orig, Level level) {
         if (Instance.State != State.None) {
+            UpdateBackdropWhenWaiting(level);
+        }
+        else {
+            orig(level);
+        }
+        OnAfterUpdate_EvenIfGameFreeze?.Invoke(level);
+
+        static void UpdateBackdropWhenWaiting(Level level) {
             level.Wipe?.Update(level);
             level.HiresSnow?.Update(level);
             level.Foreground.Update(level);
             level.Background.Update(level);
             level.Tracker.GetEntity<Tooltip>()?.Update();
             level.Tracker.GetEntity<NonFrozenMiniTextbox>()?.Update();
-            return;
         }
-
-        orig(level);
     }
 
     private static readonly Lazy<PropertyInfo> InGameOverworldHelperIsOpen = new(